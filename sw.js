88405ea444f0f95479d0d1d322c51745e6858057
lincolnpontes <lincolnpontes@gmail.com>
2026-05-25 10:47:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'alo-rfb-v1.2.1';
+const CACHE_NAME = 'alo-rfb-v1.2.2';
 const ASSETS = [ './', './index.html', './manifest.json' ];
 
 self.addEventListener('install', event => {
